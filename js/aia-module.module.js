28d47624e70c13d3f2d265d0c394483d39f3c11a
HuyLT3 <huylt0208@gmail.com>
2017-08-31 17:00:25 +0700

    add toolbar v.01

--- js/aia-module.module.js
+++ js/aia-module.module.js
@@ -1,3 +1,4 @@
 angular.module('aia-module', [
-		'search'
+		'search',
+		'toolBar'
 	]);
---
+++ js/tool-bar/tool-bar.component.js
@@ -0,0 +1,5 @@
+angular.
+	module('toolBar').
+	component('toolBar', {
+		templateUrl : 'js/tool-bar/tool-bar.template.html'
+	});
